b23e009654ecd96c88f5b7571256e31af3866717
Kn-weather <kinshinow@gmail.com>
2026-08-09 12:50:34 -0400

    chore: bump cache version to v92 (satellite blank frame fix)

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    - Map tiles (cartocdn.com, tilecache.rainviewer.com): network-only
      (too many to cache, and they update frequently) */
 
-var CACHE_NAME='skywarn-us-v94';
+var CACHE_NAME='skywarn-us-v95';
 var STATIC_ASSETS=[
   './manifest.json',
   './skywarn-logo.png',
